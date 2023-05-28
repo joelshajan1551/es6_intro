3ce7cad43c274efa1ef90b39f6329b99917148a0
joelshajan1551 <joelshajan0000@gmail.com>
2023-05-28 12:35:29 +0530

    trailing commas

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import { Animal, Cat } from "./animal.js";
+// import { Animal, Cat } from "./animal.js";
 
-let cat = new Cat("cat", 4);
+// let cat = new Cat("cat", 4);
 
-cat.makenoise();
+// cat.makenoise();
 
-console.log(cat.metaData);
+// console.log(cat.metaData);
+
+function add(param2,) {
+  const example = {
+    name: " Dylan",
+  };
+  console.log(example);
+}
+
+add();

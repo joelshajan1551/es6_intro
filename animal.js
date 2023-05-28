99b5df157aed027fa1d7bb6c15333edb1198108e
joelshajan1551 <joelshajan0000@gmail.com>
2023-05-28 12:27:29 +0530

    classes

---
+++ animal.js
@@ -0,0 +1,30 @@
+export class Animal {
+  constructor(type, legs) {
+    this.type = type;
+    this.legs = legs;
+  }
+
+  makenoise(sound = "loud") {
+    console.log(sound);
+  }
+  get metaData() {
+    return `Type: ${this.type}, legs: ${this.legs}`;
+  }
+
+  static return10() {
+    return 10;
+  }
+}
+
+// export class Cat extends Animal {
+//   constructor(type, legs, tail) {
+//     super(type, legs);
+//     this.tail = tail;
+//   }
+// }
+
+export class Cat extends Animal {
+  makenoise(sound = " meow") {
+    console.log(sound);
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import { Animal, Cat } from "./animal.js";
+
+let cat = new Cat("cat", 4);
+
+cat.makenoise();
+
+console.log(cat.metaData);
